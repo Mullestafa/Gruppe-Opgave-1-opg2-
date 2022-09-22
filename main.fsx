24f1128ef67f516d32d94011e560a0180d604319
Mullestafa <mustafahekmat@hotmail.dk>
2022-09-22 13:08:46 +0200

    Added rotation

--- main.fsx
+++ main.fsx
@@ -7,4 +7,6 @@ type vec = float * float
 let vecMull (f:float, (v1:float, v2:float):vec) : vec =
     (v1*f,v2*f)
 
-printf "%A" (vecMull(2,(3,4)))
+let vecRotate (r:float, (v1,v2):vec) : vec =
+    (v1*cos(r)-v2*sin(r),v1*sin(r)+v2*cos(r))
+printf "%A" (vecRotate((System.Math.PI),(3.0,0.0)))
