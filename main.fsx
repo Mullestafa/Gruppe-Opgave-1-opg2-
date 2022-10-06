3a9d37f3e952337b001ced11fe982a87eb9c5c28
Niller84 <114150674+Niller84@users.noreply.github.com>
2022-10-06 13:07:45 +0200

    Update main.fsx

--- main.fsx
+++ main.fsx
@@ -4,14 +4,6 @@ open Canvas
 
 type vec = float * float
 
-
-
-
-
-
-let vecSum (x1:float, y1:float) (x2:float, y2:float) : vec = 
-    (x1 + x2, y1 + y2)
-
 /// <summary>
 /// Given parameters f, and v solve for fv
 /// when v=(x,y) then ouput (f*x,f*y)
@@ -19,7 +11,7 @@ let vecSum (x1:float, y1:float) (x2:float, y2:float) : vec =
 /// <param name="f">coefficient</param>
 /// <param name="v">vector</param>
 /// <returns>New vector f*v.</returns>
-let vecMull (f:float) (v1:float, v2:float): vec =
+let vecMull (f:float, (v1:float, v2:float):vec) : vec =
     (v1*f,v2*f)
 
 /// <summary>
@@ -28,7 +20,8 @@ let vecMull (f:float) (v1:float, v2:float): vec =
 /// <param name="r">radians clockwise</param>
 /// <param name="v">vector</param>
 /// <returns>New vector rotated r radians clockwise.</returns>
-let vecRotate (r:float) (v1,v2) : vec =
+let vecRot (r:float, (v1,v2):vec) : vec =
     (v1*cos(r)-v2*sin(r),v1*sin(r)+v2*cos(r))
 
 let toInt ((v1:float, v2:float):vec) : int*int =
+    
