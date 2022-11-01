796f87b64bde79e5821a306f49d201ce6d4bd92d
Jakup Lutzen <jakup@lutzen.co>
2022-11-01 12:02:31 +0100

    remove empty

--- 6g/2048/Program.fs
+++ 6g/2048/Program.fs
@@ -3,7 +3,7 @@ open System
 
 
 type pos = int*int
-type value = Red | Green | Blue | Yellow | Black | Empty
+type value = Red | Green | Blue | Yellow | Black
 type piece = value * pos
 type state = piece list
 
