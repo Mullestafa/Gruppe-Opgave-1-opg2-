fb6da399107eb182be378568442fec4a26382033
Niller84 <114150674+Niller84@users.noreply.github.com>
2022-12-01 13:10:25 +0100

    Final

--- 6g/2048/Program.fsx
+++ 6g/2048/Program.fsx
@@ -1,24 +1,5 @@
 open Game
 
-let testSetA = 
-    ([(Red ,(1,0)); (Red ,(1,1)); (Red ,(1,2)); (Blue ,(0,1)); (Green ,(2,0))], // The starting board
-    [(Green ,(1,0)); (Red ,(1,1)); (Blue ,(0,0)); (Green ,(2,0))],              // Expected result from shiftUp
-    [(Red ,(1,0)); (Red ,(1,1)); (Red ,(1,2)); (Blue ,(0,1)); (Green ,(2,2))],  // Expected result from flipUD
-    [(Red ,(0,1)); (Red ,(1,1)); (Red ,(2,1)); (Blue ,(1,0)); (Green ,(0,2))],  // Expected result from transpose
-    [(0,0);(0,2);(2,1);(2,2)])                                                  // Expected result from empty
-
-let testSetEmpty = 
-    ([],                                                        // The starting board
-    [],                                                         // Expected result from shiftUp
-    [],                                                         // Expected result from flipUD
-    [],                                                         // Expected result from transpose
-    [(0,0);(0,1);(0,2);(1,0);(1,1);(1,2);(2,0);(2,1);(2,2)])    // Expected result from empty
-
-let a = unitTest testSetA "testSetA"
-let b = unitTest testSetEmpty "Empty set"
-printfn "ALL SETS COMPLETED SUCCESSFULLY:.. %b" <| (a=b)
-
-
 let draw (h:int) (w:int) (s:state) =
     let C = Canvas.create (w:int) (h:int)
     let pieceSize = h / 3
@@ -32,18 +13,22 @@ let draw (h:int) (w:int) (s:state) =
 let react (s:state) (k:Canvas.key) : (state option) =
     match Canvas.getKey k with
     | Canvas.LeftArrow ->
-        s |> transpose |> shiftUp |> transpose |> addRandom Red
-
+        let state = transpose s |> shiftUp |> transpose
+        let state = addRandom Game.Red state |> fun (Some x) -> x
+        Some state
     | Canvas.RightArrow ->
-        s |> transpose |> flipUD |> shiftUp |> flipUD |> transpose |> addRandom Red
-
+        let state = transpose s |> flipUD |> shiftUp |> flipUD |> transpose
+        let state = addRandom Game.Red state |> fun (Some x) -> x
+        Some state
     | Canvas.UpArrow ->
-        s |> shiftUp |> addRandom Red
-
+        let state = s |> shiftUp
+        let state = addRandom Game.Red state |> fun (Some x) -> x
+        Some state
     | Canvas.DownArrow ->
-        s |> flipUD |> shiftUp |> flipUD |> addRandom Red
+        let state = s |> flipUD |> shiftUp |> flipUD
+        let state = addRandom Game.Red state |> fun (Some x) -> x
+        Some state    
     | _ -> None
 
 
-
-do Canvas.runApp "2048" 600 600 draw react ([] |> recklessAdd Red |> recklessAdd Blue)
+do Canvas.runApp "2048" 600 600 draw react (addRandom Blue (addRandom Red []|> fun (Some x) -> x)|> fun (Some x) -> x)
