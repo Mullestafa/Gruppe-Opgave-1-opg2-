376a6a9d4b13bf2e4ed783539af02b2607af16b2
Mullestafa <mustafahekmat@hotmail.dk>
2022-12-01 16:38:56 +0100

    8g finished!? (hopefully)

--- 8g/CyclicQueue/CyclicQueue.fs
+++ 8g/CyclicQueue/CyclicQueue.fs
@@ -11,14 +11,16 @@ let create (n:int) : unit =
 
 let iterate i:int option =
     match i with
-    Some x when x = (queue.Length - 1) -> Some 0
+    Some x when x = (queue.Length-1) -> Some 0
     | Some (x: int) -> Some (x+1)
     | None -> None
 
-let isfull: bool =
+let isfull (): bool =
     match last with
-    Some x when iterate (Some x) = first -> true
-    | _ -> false
+    Some x when (iterate last) = first ->
+        true
+    | _ -> 
+        false
 
 let enqueue (e:Value) : bool =
     match last with
@@ -28,7 +30,7 @@ let enqueue (e:Value) : bool =
         first <- Some 0
         true
     | _ ->
-        if isfull then
+        if isfull() then
             false
         else
             last <- (iterate last)
@@ -60,22 +62,23 @@ let length () : int =
     match last with
     None -> 0
     | Some x -> 
-        if last > first then
+        if last.Value > first.Value then
             last.Value - first.Value + 1
         else
             (x+1) + (queue.Length-first.Value+1)
 
-let classicQue : Value option[] =
+let classicQue () : Value option[] =
     match last with
     None -> [||]
     | Some x -> 
-        if last > first then
-            queue[last.Value..first.Value]
+        if last.Value > first.Value then
+            queue[first.Value..last.Value]
         else
-            Array.append queue[0..last.Value] queue[first.Value..(queue.Length-1)]
+            Array.append queue[first.Value..(queue.Length-1)] queue[0..last.Value]
 
 let toString () : string =
-    let filteredArr: Value option[] = classicQue
+    let filteredArr: Value option[] = classicQue()
     let stringArr: string[] = Array.map (fun (i:Value option) -> string(i.Value)) filteredArr
-    Array.fold (fun (acc: string) (x: string) -> acc + "," + x) "" stringArr
+    let strArr: string = Array.fold (fun (acc: string) (x: string) -> acc + x + " ") "" stringArr
+    strArr.Trim()
  
--- 8g/CyclicQueue/CyclicQueueApp.fs
+++ 8g/CyclicQueue/CyclicQueueApp.fs
@@ -5,7 +5,17 @@ let main _ =
     // Write your tests here
     // (or organize your tests into functions and call them from here)
     // Exit status; consider making it the number of failed tests
+    create 10
+    let mutable errorCount = 0
+    errorCount <- errorCount + (if List.map (fun i -> enqueue i) [1..11] = [true; true; true; true; true; true; true; true; true; true; false] then 0 else 1) // testing enqueu
+    
+    errorCount <- errorCount + (if List.map (fun i -> dequeue()) [1..5] = [Some 1; Some 2; Some 3; Some 4; Some 5] then 0 else 1)  // testing dequeue
 
+    errorCount <- errorCount + (if List.map (fun i -> enqueue i) [1..5] =  [true; true; true; true; true] then 0 else 1)// testing circular iteration while enqueing
 
+    errorCount <- errorCount + (if List.map (fun i -> dequeue()) [1..11] = [Some 6; Some 7; Some 8; Some 9; Some 10; Some 1; Some 2; Some 3; Some 4; Some 5; None] then 0 else 1) // testing circular iteration
 
-    0
+    errorCount <- errorCount + (if List.map (fun i -> enqueue i) [1..10] = [true; true; true; true; true; true; true; true; true; true] then 0 else 1) // testing repopulating a reinitialized test
+    
+    printfn "error count: %A" errorCount
+    errorCount
